a4ec8771a654e1e65698745a8a2cc3088b79cf15
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-05-17 11:16:10 +0900

    [unrated] Title: 배열 조각하기, Time: 1.79 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        ArrayList<Integer> arrL = new ArrayList<Integer>();
+
+		for (int i : arr) {
+			arrL.add(i);
+		}
+
+		for (int i = 0; i < query.length; i++) {
+			if (i % 2 == 0) {
+				while (arrL.size() != query[i] + 1) {
+					arrL.remove(query[i] + 1);
+				}
+			} else {
+				for (int j = 0; j < query[i]; j++) {
+					arrL.remove(0);
+				}
+			}
+		}
+
+		int answer[] = new int[arrL.size()];
+		for (int i = 0; i < arrL.size(); i++) {
+			answer[i] = arrL.get(i);
+		}
+
+        return answer;
+    }
+}
