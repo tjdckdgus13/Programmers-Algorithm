78717b0d7fb4b49e0844d7ec2e46ff7f51079057
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-05-21 00:06:59 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int a = x;
+        int sum = 0;
+        while(a != 0) {
+            sum += a % 10;
+            a /= 10;
+        }
+        
+        if (x % sum == 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
