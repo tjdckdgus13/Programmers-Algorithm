ca446a53f3150b8bec96b2ea6f294cfd446242b1
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-06-03 23:21:07 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+    
+        int a = num_list[num_list.length - 1];
+		int b = num_list[num_list.length - 2];
+		int[] answer = new int[num_list.length + 1];
+		for (int i = 0; i < num_list.length; i++) {
+			answer[i] = num_list[i];
+		}
+
+		answer[answer.length - 1] = a > b ? a - b : a * 2;
+        return answer;
+    }
+}
