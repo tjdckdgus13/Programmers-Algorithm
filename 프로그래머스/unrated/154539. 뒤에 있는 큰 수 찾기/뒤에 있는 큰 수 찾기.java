dfbe7a09db28fcd467e52e408aa2e325471b85fb
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-07-05 21:21:03 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 88.41 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -4,31 +4,31 @@ class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
 
-		Stack<int[]> stack = new Stack<>();
+        Stack<int[]> stack = new Stack<>();
 
-		stack.add(new int[] { 0, numbers[0] });
-		for (int i = 1; i < numbers.length; i++) {
-			while (true) {
-				if (stack.size() == 0) {
-					stack.add(new int[] { i, numbers[i] });
-					break;
-				}
-				int[] a = stack.peek();
-				if (a[1] >= numbers[i]) {
-					stack.add(new int[] { i, numbers[i] });
-					break;
-				} else {
-					answer[a[0]] = numbers[i];
-					stack.pop();
-				}
-			}
-		}
+        stack.add(new int[] { 0, numbers[0] });
+        for (int i = 1; i < numbers.length; i++) {
+            while (true) {
+                if (stack.size() == 0) {
+                    stack.add(new int[] { i, numbers[i] });
+                    break;
+                }
+                int[] a = stack.peek();
+                if (a[1] >= numbers[i]) {
+                    stack.add(new int[] { i, numbers[i] });
+                    break;
+                } else {
+                    answer[a[0]] = numbers[i];
+                    stack.pop();
+                }
+            }
+        }
 
-		int count = stack.size();
-		for (int j = 0; j < count; j++) {
-			int[] a = stack.pop();
-			answer[a[0]] = -1;
-		}
+        int count = stack.size();
+        for (int j = 0; j < count; j++) {
+            int[] a = stack.pop();
+            answer[a[0]] = -1;
+        }
         return answer;
     }
 }
