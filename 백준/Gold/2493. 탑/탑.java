9d131775b58990e2c3030cf30276590cb7bcfb92
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2023-07-12 19:03:48 +0900

    [Gold V] Title: 탑, Time: 808 ms, Memory: 100960 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,61 +1,55 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		Stack<Top> s = new Stack<Top>();
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Stack<Top> stack = new Stack<>();
 		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(st.nextToken());
 
-			if (s.isEmpty()) {
-				sb.append("0 ");
-				s.push(new Top(i, num));
+			if (stack.isEmpty()) {
+				sb.append(0 + " ");
+				stack.add(new Top(num, i + 1));
 			} else {
 				while (true) {
-					if (s.isEmpty()) {
-						sb.append("0 ");
-						s.push(new Top(i, num));
+					if (stack.isEmpty()) {
+						sb.append(0 + " ");
+						stack.add(new Top(num, i + 1));
 						break;
 					}
 
-					Top top = s.peek();
+					Top top = stack.peek();
 
 					if (top.height > num) {
-						sb.append(top.num + " ");
-						s.push(new Top(i, num));
+						sb.append(top.idx + " ");
+						stack.add(new Top(num, i + 1));
 						break;
 					} else {
-						s.pop();
+						stack.pop();
 					}
 				}
 			}
 		}
-		System.out.println(sb);
-
+		System.out.println(sb.toString());
 	}
 
 }
 
 class Top {
-
-	int num;
 	int height;
+	int idx;
 
-	Top(int num, int height) {
-		this.num = num;
+	Top(int height, int idx) {
 		this.height = height;
+		this.idx = idx;
 	}
 }
