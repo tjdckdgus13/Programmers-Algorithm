380716652a3c1e78397a0f31679f6223bbb851d2
tjdckdgus13 <82072933+tjdckdgus13@users.noreply.github.com>
2025-03-27 23:42:58 +0900

    [level 1] Title: 유연근무제, Time: 0.73 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = schedules.length;
+        
+        for (int i=0; i<schedules.length; i++) {
+            for (int j=0; j<7; j++) {
+                if ((startday + j)%7>0 && (startday + j)%7<6
+                   && 60*(timelogs[i][j]/100)+timelogs[i][j]%100>60*(schedules[i]/100)+schedules[i]%100+10) {
+                    answer--;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
